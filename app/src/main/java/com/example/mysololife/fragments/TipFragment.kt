4732563d59dde097bab676d526f4008b5be33e60
DoubleJ <131241458+doit-zero@users.noreply.github.com>
2025-03-08 23:36:34 +0900

    네비게이션 구현

--- app/src/main/java/com/example/mysololife/fragments/TipFragment.kt
+++ app/src/main/java/com/example/mysololife/fragments/TipFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mysololife.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.mysololife.R
+import com.example.mysololife.contentslist.ContentListActivity
 import com.example.mysololife.databinding.FragmentTipBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -33,6 +35,10 @@ class TipFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_tip, container, false)
 
+        binding.category1.setOnClickListener{
+            var intent = Intent(context,ContentListActivity::class.java)
+            startActivity(intent)
+        }
         binding.homeTap.setOnClickListener {
             it.findNavController().navigate(R.id.action_tipFragment_to_homeFragment)
         }
